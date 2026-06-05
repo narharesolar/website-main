9adabcac230afb0598b18552e5a093dd6e24a733
Tushit Sharma <tushit08sharma@gmail.com>
2026-06-06 00:54:44 +0530

    Solved vercel error

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ba506edae0f4a55"; }
+/******/ 		__webpack_require__.h = function() { return "d7201b26afd64934"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
