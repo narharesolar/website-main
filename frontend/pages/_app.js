e9fabe11c841378c440c1a5ada23f948f2e82bf6
Tushit Sharma <tushit08sharma@gmail.com>
2026-08-09 17:13:50 +0530

    facebook pixel update

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,9 +1,11 @@
 import "../styles/globals.css";
 import { LanguageProvider } from "../context/LanguageContext";
+import FacebookPixel from "../components/FacebookPixel";
 
 export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
+      <FacebookPixel />
       <Component {...pageProps} />
     </LanguageProvider>
   );
